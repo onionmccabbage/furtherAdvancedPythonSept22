9aa81dc17b58e0522d5aef23511b74b1f9f934e9
onionmccabbage <onionmccabbage@live.com>
2022-09-27 10:59:27 +0100

    Add files via upload

--- using_threads/06_locks.py
+++ using_threads/06_locks.py
@@ -9,11 +9,36 @@ def workerA():
     global counter # we need access to the global asset
     lock.acquire()
     try:
-        while counter <100:
+        while counter <100: # try to increment up to +100
             counter += 1 # increment
-            print('Worker A increments coutnerr to {}'.format(counter))
+            print('Worker A increments counter to {}'.format(counter))
     except Exception as e:
         print(e)
     finally:
         lock.release() # release the lock when we no longer need it
-        
+        pass
+
+def workerB():
+    global counter # we need access to the global asset
+    lock.acquire()
+    try:
+        while counter >-100: # try to decrement all the way to -100
+            counter -= 1 # decrement
+            print('Worker B decrements counter to {}'.format(counter))
+    except Exception as e:
+        print(e)
+    finally:
+        lock.release() # release the lock when we no longer need it   
+        pass
+
+if __name__ == '__main__':
+    # we can measure how long code takes to execute
+    t0 = time.time()
+    thread1 = threading.Thread(target=workerA)
+    thread2 = threading.Thread(target=workerB)
+    thread2.start()
+    thread1.start() # it can make a difference the order in which locked threads are started
+    thread1.join()
+    thread2.join()
+    t1 = time.time()
+    print('total execution time {} seconds'.format(t1-t0)) # always a good idea to take repeated measurements
