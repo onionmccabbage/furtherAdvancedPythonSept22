63b01f719d8296f6fcd2594c809b22302c1cc056
onionmccabbage <onionmccabbage@live.com>
2022-09-28 15:40:48 +0100

    Add files via upload

---
+++ review3/weather_app/weather.py
@@ -0,0 +1,48 @@
+from threading import Thread
+import json
+import requests
+import time
+
+class TempGetter(Thread):
+    def __init__(self, city):
+        """
+        The __init__ method initializes the TempGetter class
+        Takes a 'city' parameter
+        """
+        super().__init__()
+        self.city = city
+        self.temperature = -99
+
+    def run(self):
+        """
+        The run method is invoked when this class is the target of a thread
+        """
+        url_template = (
+            'http://api.openweathermap.org/data/2.5/'
+            'weather?q={}&units=metric&APPID=48f2d5e18b0d2bc50519b58cce6409f1')
+        response = requests.get(url_template.format(self.city))
+        data = response.json()
+        self.temperature = data['main']['temp']
+
+def getWeather(cities=['athlone', 'galway']):
+    """
+    the getWeather method takes a list of cities and returns the temperature for each city.
+
+    """
+    threads = [TempGetter(c) for c in cities]
+    start = time.time()
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    for thread in threads:
+        return (thread.temperature)
+
+# if module is run as a unit, execute tests
+if __name__ == '__main__':
+    import doctest
+    # doctest.testmod(verbose = True) # comment out for profiling
+    x = getWeather()
+    print(x)
